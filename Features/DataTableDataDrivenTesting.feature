cc8789f7926f2ddaf2d6eab1c549d37d5e3c5049
Prashant Shivaji Jadhav <125643307+Prashantjadhav2620@users.noreply.github.com>
2023-12-20 16:25:08 +0530

    insert Page Object Model

---
+++ Features/DataTableDataDrivenTesting.feature
@@ -0,0 +1,12 @@
+﻿Feature: DataTableDataDrivenTesting Data Driven Testing Youtube seearch with parameter
+
+DataTable Data DrivenTesting Data Driven Testing Youtube seearch with parameter the feature
+
+@testerTalk
+Scenario:DataTableDataDrivenTesting Data Driven Testing Youtube seearch with parameter
+	Given Open the browser
+	When Enter the URL
+	Then Enter search keyword in the Youtube
+	|		 searchkey				   |
+	|  Tester talk by prashant  usingDataTable       |
+	|  Tester talk by selenium  usingDataTable       |
---
+++ Pages/SearchPage.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecFlowProject1.Pages
+{
+    public class SearchPage
+    {
+        private IWebDriver driver;
+
+        public SearchPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        By SearchTextBox = By.XPath("//*[@name='search_query']");
+
+        public ResultPage searchText(String text) {
+            
+            driver.FindElement(SearchTextBox).SendKeys(text);
+            driver.FindElement(SearchTextBox).SendKeys(Keys.Enter);
+            //Thread.Sleep(5000);
+            return new ResultPage(driver);
+        }
+
+    }
+}
